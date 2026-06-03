cd501706296c6747a8845d131d9529e442bf77a4
abhinav <abhinavpuliyeri@gmail.com>
2026-06-03 10:53:19 +0530

    Add files via upload

---
+++ unitconvertor/index.js
@@ -0,0 +1,38 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const convertBtn = document.getElementById("convert")
+const lengthEl = document.getElementById("len-el")
+const volumeEl = document.getElementById("vol-el")
+const massEl = document.getElementById("mass-el")
+const inputEl = document.getElementById("input")
+
+const meterToFeet =  3.281
+const literToGallon =  0.264
+const kiloToPound =  2.204
+
+convertBtn.addEventListener("click", function() {
+    let baseValue = Number(inputEl.value)
+
+    let feet = (baseValue * meterToFeet).toFixed(3)
+    let meters = (baseValue / meterToFeet).toFixed(3)
+
+    lengthEl.textContent = `${baseValue} meters = ${feet} feet | ${baseValue} feet = ${meters} meters`
+
+    let liters = (baseValue * literToGallon).toFixed(3)
+    let gallons = (baseValue / literToGallon).toFixed(3)
+
+
+    volumeEl.textContent = `${baseValue} liters = ${gallons} gallons | ${baseValue} gallons = ${liters} liters`
+    
+
+    let kilo = (baseValue * kiloToPound).toFixed(3)
+    let pound = (baseValue / kiloToPound).toFixed(3)
+  
+    massEl.textContent = `${baseValue} kilo = ${pound} pound | ${baseValue} pound = ${kilo} kilo`
+
+
+})
