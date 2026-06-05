4731d0030842740782e4a8cdc9b4f1302095f57f
abhinav <abhinavpuliyeri@gmail.com>
2026-06-05 10:58:36 +0530

    Remove commented conversion factors and clean code

--- unitconvertor/index.js
+++ unitconvertor/index.js
@@ -1,9 +1,3 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-
 const convertBtn = document.getElementById("convert")
 const lengthEl = document.getElementById("len-el")
 const volumeEl = document.getElementById("vol-el")
